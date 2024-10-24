3038fe5b2d488b9cafe6b82ae13a52d2a7932763
Daniel Romero <130934263+Daniromero1410@users.noreply.github.com>
2024-10-24 13:14:14 -0500

    Inicializacion del Proyecto

---
+++ accounts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('login')
+
+@login_required
+def home(request):
+    return render(request, 'accounts/home.html')
